d561e3e5846eafef5821eec69fc1a5c48cb50842
Adonis Mendoza <donimen1@gmail.com>
2024-01-19 16:47:30 -0400

    Findin ideas for the stack

---
+++ src/services/stack.ts
@@ -0,0 +1,34 @@
+const stack = [
+  {
+    name: "JavaScript",
+    color: "rgba(226, 236, 204,50%)",
+  },
+  {
+    name: "TypeScript",
+    color: "rgba(3, 223, 203,50%)",
+  },
+  {
+    name: "React Js",
+    color: "rgb(226, 215, 83,50%)",
+  },
+  {
+    name: "Vue",
+    color: "rgb(208, 2, 7,50%)",
+  },
+  {
+    name: "Node",
+    color: "rgb(5, 11, 43,50%)",
+  },
+  {
+    name: "Express",
+    color: "rgb(252, 1, 7,50%)",
+  },
+  {
+    name: "Adonis Js",
+    color: "rgb(208, 2, 7)",
+  },
+];
+
+export const getStack = () => {
+  return stack;
+};
